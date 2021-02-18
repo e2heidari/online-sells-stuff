8f50c60ea6fc9c6d7d21e683fd49e6f1eaf990df
Ehsan_Heidari <ehsan.heydari@gmail.com>
2021-02-18 15:42:47 -0800

    add cart stuff counter

--- src/components/Cart/CartStuff.js
+++ src/components/Cart/CartStuff.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./CartStuff.css";
 import Button from "@material-ui/core/Button";
 import HandelAmount from "./HandelAmount";
@@ -12,7 +12,21 @@ const CartStuff = ({
   index,
   removeStuff,
 }) => {
-  console.log(title);
+  const [amountStuff, setAmountStuff] = useState(1);
+  const handelClickUp = () => {
+    setAmountStuff(amountStuff + 1);
+  };
+  const handelClickDown = () => {
+    setAmountStuff(amountStuff - 1);
+    if (amountStuff <= 1) {
+      setAmountStuff(1);
+    }
+  };
+  const [priceWithAmount, setPriceWithAmount] = useState(price);
+  useEffect(() => {
+    setPriceWithAmount(amountStuff * price);
+  }, [amountStuff]);
+
   return (
     <div className="container">
       <h2 className="item-a">{title}</h2>
@@ -28,7 +42,14 @@ const CartStuff = ({
         Remove from Cart
       </Button>
       <h1 className="item-e">${price}</h1>
-      <HandelAmount price={price} />
+      <div className="item-f">
+        <HandelAmount
+          handelClickUp={handelClickUp}
+          handelClickDown={handelClickDown}
+          amountStuff={amountStuff}
+        />
+      </div>
+      <h1 className="item-g">${priceWithAmount}</h1>
     </div>
   );
 };
--- src/components/Cart/HandelAmount.js
+++ src/components/Cart/HandelAmount.js
@@ -1,23 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import IconButton from "@material-ui/core/IconButton";
-import ExpandLessIcon from "@material-ui/icons/ExpandLess";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import RemoveCircleOutlineIcon from "@material-ui/icons/RemoveCircleOutline";
 import "./HandelAmount.css";
 
-const HandelAmount = ({ price }) => {
-  const [amountStuff, setAmountStuff] = useState(1);
+const HandelAmount = ({ handelClickUp, handelClickDown, amountStuff }) => {
   return (
     <div>
       <span className="countContaner">
-        <IconButton>
-          <ExpandLessIcon />
+        <IconButton onClick={() => handelClickUp()}>
+          <AddCircleOutlineIcon />
         </IconButton>
         <input value={amountStuff} size="4" className="countStuff" />
-        <IconButton>
-          <ExpandMoreIcon />
+        <IconButton onClick={() => handelClickDown()}>
+          <RemoveCircleOutlineIcon />
         </IconButton>
       </span>
-      ${price}
     </div>
   );
 };
